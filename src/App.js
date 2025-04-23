200e3a6b7e4c9490bdd40160ff66a989aff5b62c
felixngfender <ngthinh302@gmail.com>
2025-04-23 15:01:16 -0400

    add agent demo to test whole flow

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import './App.css';
-import TopPanel from './TopPanel';
-import BottomPanel from './BottomPanel'
+import "./App.css";
+import AgentDemo from "./AgentDemo";
+import TopPanel from "./TopPanel";
+import BottomPanel from "./BottomPanel";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-    
-        Squidbox
-        
-
-      </header>
+      <header className="App-header">Squidbox</header>
 
       <TopPanel />
 
       <BottomPanel />
 
+      {false && <AgentDemo />}
     </div>
-
-    
-
-
   );
 }
 
